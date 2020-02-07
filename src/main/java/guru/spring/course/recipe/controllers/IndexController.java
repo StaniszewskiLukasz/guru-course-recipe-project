2ab41f0a84ca88e99a47956310b7fd0ad72e3742
Łukasz Staniszewski <2277lukas@gmail.com>
2020-02-07 09:36:00 +0100

    Add index page and controller

---
+++ src/main/java/guru/spring/course/recipe/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package guru.spring.course.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Łukasz Staniszewski on 2020-02-07
+ * @project recipe
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/index","","/"})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
